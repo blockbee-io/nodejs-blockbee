d5cc802101457d84571553dd2dfdfb39b9510a98
arianoangelo <geral@arianoangelo.com>
2024-03-18 20:50:48 +0000

    v2.0.0  * Automated Payouts  * Support to BlockBee Checkout page  * Various improvements

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /**
- * BlockBee's NodeJS Library
+ * BlockBee's Node.js Library
  * @author BlockBee <info@blockbee.io>
  */
 class BlockBee {
     static #baseURL = 'https://api.blockbee.io'
 
-    constructor(coin, ownAddress, callbackUrl, parameters = {}, bbParams = {}, apiKey) {
-        if (!apiKey) {
+    constructor( coin, ownAddress, callbackUrl, parameters = {}, bbParams = {}, apiKey ) {
+        if ( !apiKey ) {
             throw new Error('Missing API Key')
         }
 
         BlockBee.getSupportedCoins(apiKey).then(validCoins => {
-            if (!validCoins.hasOwnProperty(coin)) {
+            if ( !validCoins.hasOwnProperty(coin) ) {
                 throw new Error('The cryptocurrency/token requested is not supported.')
             }
         })
@@ -30,10 +30,10 @@ class BlockBee {
      * @param apiKey
      * @returns {Promise<{}|null>}
      */
-    static async getSupportedCoins(apiKey) {
+    static async getSupportedCoins( apiKey ) {
         const info = await this.getInfo(null, true, apiKey)
 
-        if (!info) {
+        if ( !info ) {
             return null
         }
 
@@ -41,15 +41,15 @@ class BlockBee {
 
         const coins = {}
 
-        for (const chain of Object.keys(info)) {
+        for ( const chain of Object.keys(info) ) {
             const data = info[chain]
             const isBaseCoin = data.hasOwnProperty('ticker')
 
-            if (isBaseCoin) {
+            if ( isBaseCoin ) {
                 coins[chain] = data
             } else {
                 const baseTicker = `${chain}_`
-                Object.entries(data).forEach(([token, subData]) => {
+                Object.entries(data).forEach(( [token, subData] ) => {
                     coins[baseTicker + token] = subData
                 })
             }
@@ -64,32 +64,34 @@ class BlockBee {
      * @returns {Promise<*|null>}
      */
     async getAddress() {
-        if (!this.coin || !this.callbackUrl || !this.apiKey) {
+        if ( !this.coin || !this.callbackUrl || !this.apiKey ) {
             return null
         }
 
         let callbackUrl = new URL(this.callbackUrl)
         const parameters = this.parameters
-    
-        if (Object.entries(parameters).length > 0) {
-            Object.entries(parameters).forEach(([k, v]) => callbackUrl.searchParams.append(k, v))
+
+        if ( Object.entries(parameters).length > 0 ) {
+            Object.entries(parameters).forEach(( [k, v] ) => callbackUrl.searchParams.append(k, v))
         }
-        
+
         const params = this.ownAddress
-            ? {...this.bbParams, ...{
+            ? {
+                ...this.bbParams, ...{
                     callback: encodeURI(callbackUrl.toString()),
                     address: this.ownAddress,
                     apikey: this.apiKey
                 }
-            } : {...this.bbParams, ...{
+            } : {
+                ...this.bbParams, ...{
                     callback: encodeURI(callbackUrl.toString()),
                     apikey: this.apiKey
                 }
             }
 
-        const response = await BlockBee.#_request(this.coin, 'create', params)
+        const response = await BlockBee.#_request_get(this.coin, 'create', params)
 
-        if (response.status === 'success') {
+        if ( response.status === 'success' ) {
             const addressIn = response.address_in
 
             this.paymentAddress = addressIn
@@ -105,15 +107,15 @@ class BlockBee {
      * @returns {Promise<any|null>}
      */
     async checkLogs() {
-        if (!this.coin || !this.callbackUrl) {
+        if ( !this.coin || !this.callbackUrl ) {
             return null
         }
 
         let callbackUrl = new URL(this.callbackUrl)
         const parameters = this.parameters
-    
-        if (Object.entries(parameters).length > 0) {
-            Object.entries(parameters).forEach(([k, v]) => callbackUrl.searchParams.append(k, v))
+
+        if ( Object.entries(parameters).length > 0 ) {
+            Object.entries(parameters).forEach(( [k, v] ) => callbackUrl.searchParams.append(k, v))
         }
 
         callbackUrl = encodeURI(callbackUrl.toString())
@@ -123,9 +125,9 @@ class BlockBee {
             apikey: this.apiKey
         }
 
-        const response = await BlockBee.#_request(this.coin, 'logs', params)
+        const response = await BlockBee.#_request_get(this.coin, 'logs', params)
 
-        if (response.status === 'success') {
+        if ( response.status === 'success' ) {
             return response
         }
 
@@ -138,21 +140,27 @@ class BlockBee {
      * @param size
      * @returns {Promise<any|null>}
      */
-    async getQrcode(value = null, size = 512) {
+    async getQrcode( value = null, size = 512 ) {
+        let address = this.paymentAddress
+
+        if ( !address ) {
+            address = await this.getAddress()
+        }
+
         const params = {
-            address: this.paymentAddress,
+            address: address,
             apikey: this.apiKey
         }
 
-        if (value) {
+        if ( value ) {
             params['value'] = value
         }
 
         params['size'] = size
 
-        const response = await BlockBee.#_request(this.coin, 'qrcode', params)
+        const response = await BlockBee.#_request_get(this.coin, 'qrcode', params)
 
-        if (response.status === 'success') {
+        if ( response.status === 'success' ) {
             return response
         }
 
@@ -167,22 +175,22 @@ class BlockBee {
      * @param apiKey
      * @returns {Promise<any|null>}
      */
-    static async getInfo(coin = null, assoc = false, apiKey) {
+    static async getInfo( coin = null, assoc = false, apiKey ) {
         const params = {}
 
-        if (!apiKey) {
+        if ( !apiKey ) {
             throw new Error('API Key is Empty')
         }
 
         params['apikey'] = apiKey
 
-        if (!coin) {
+        if ( !coin ) {
             params['prices'] = 0
         }
 
-        const response = await this.#_request(coin, 'info', params)
+        const response = await this.#_request_get(coin, 'info', params)
 
-        if (!coin || response.status === 'success') {
+        if ( !coin || response.status === 'success' ) {
             return response
         }
 
@@ -197,18 +205,18 @@ class BlockBee {
      * @param priority
      * @returns {Promise<any|null>}
      */
-    static async getEstimate(coin, apiKey, addresses = 1, priority = 'default') {
-        if (!apiKey) {
+    static async getEstimate( coin, apiKey, addresses = 1, priority = 'default' ) {
+        if ( !apiKey ) {
             throw new Error('API Key is Empty')
         }
 
-        const response = await BlockBee.#_request(coin, 'estimate', {
+        const response = await BlockBee.#_request_get(coin, 'estimate', {
             addresses,
             priority,
             apikey: apiKey
         })
 
-        if (response.status === 'success') {
+        if ( response.status === 'success' ) {
             return response
         }
 
@@ -223,39 +231,238 @@ class BlockBee {
      * @param apiKey
      * @returns {Promise<any|null>}
      */
-    static async getConvert(coin, value, from, apiKey) {
-        if (!apiKey) {
+    static async getConvert( coin, value, from, apiKey ) {
+        if ( !apiKey ) {
             throw new Error('API Key is Empty')
         }
-        
-        const response = await BlockBee.#_request(coin, 'convert', {
+
+        const response = await BlockBee.#_request_get(coin, 'convert', {
             value,
             from,
             apikey: apiKey
         })
 
-        if (response.status === 'success') {
+        if ( response.status === 'success' ) {
             return response
         }
 
         return null
     }
-    
-    static async createPayout(coin, address, value, apiKey) {
-        if (!apiKey) {
-            throw new Error('API Key is Empty')
+
+    static async createPayout( coin, requests, apiKey, process = false ) {
+        if ( !requests ) {
+            throw new Error('No requests provided')
         }
-    
-        const response = await BlockBee.#_request(coin, 'payout/request/create', {
-            value,
-            address,
-            apikey: apiKey
-        })
-    
-        if (response.status === 'success') {
+
+        const body = {
+            'outputs': requests
+        }
+
+        let endpoint = 'payout/request/bulk'
+
+        if ( process ) {
+            endpoint = endpoint + '/process'
+        }
+
+        const response = await BlockBee.#_request_post(coin, endpoint, apiKey, body, true)
+        if ( response.status === 'success' ) {
             return response
         }
-    
+
+        return null
+    }
+
+    static async listPayouts( coin, status, page, apiKey, requests = false ) {
+        const params = {}
+
+        if ( status ) {
+            params.status = status
+        }
+
+        if ( page ) {
+            params.page = page
+        }
+
+        let endpoint = 'payout/list'
+
+        if ( requests ) {
+            endpoint = 'payout/request/list'
+        }
+
+        const response = await this.#_request_get(coin, endpoint, {...params, apikey: apiKey})
+
+        if ( response.status === 'success' ) {
+            return response
+        }
+
+        return null
+    }
+
+    static async getPayoutWallet( coin, apiKey, balance = false ) {
+        let wallet = await this.#_request_get(coin, 'payout/address', {apikey: apiKey})
+
+        if ( wallet.status !== 'success' ) {
+            return null
+        }
+
+        const output = {address: wallet.address}
+
+        if ( balance ) {
+            wallet = await this.#_request_get(coin, 'payout/balance', {apikey: apiKey})
+            if ( wallet.status === 'success' ) {
+                output.balance = wallet.balance
+            }
+        }
+
+        return output
+    }
+
+    static async createPayoutByIds( apiKey, ids = [] ) {
+        if ( ids.length === 0 ) {
+            throw new Error('Please provide the Payout Request(s) ID(s)')
+        }
+
+        const response = await this.#_request_post('', 'payout/create', apiKey, {request_ids: ids.join(',')})
+
+        if ( response.status === 'success' ) {
+            return response
+        }
+
+        return null
+    }
+
+    static async processPayout( apiKey, id ) {
+        const response = await this.#_request_post('', 'payout/process', apiKey, {payout_id: id})
+
+        return response.status === 'success' ? response : null
+    }
+
+    static async checkPayoutStatus( apiKey, id ) {
+        if ( !id ) {
+            throw new Error('Please provide the Payout ID')
+        }
+
+        const response = await this.#_request_post('', 'payout/status', apiKey, {payout_id: id})
+
+        if ( response.status === 'success' ) {
+            return response
+        }
+
+        return null
+    }
+
+    /**
+     * Requests a Payment Link
+     * @returns {Promise<*|null>}
+     */
+    static async paymentRequest( redirectUrl, notifyUrl, value, apiKey, params = {}, bbParams = {} ) {
+        if ( !notifyUrl || !redirectUrl || !value || !apiKey ) {
+            return null
+        }
+
+        redirectUrl = new URL(redirectUrl)
+        notifyUrl = new URL(notifyUrl)
+
+        if ( Object.entries(params).length > 0 ) {
+            Object.entries(params).forEach(( [k, v] ) => redirectUrl.searchParams.append(k, v))
+            Object.entries(params).forEach(( [k, v] ) => notifyUrl.searchParams.append(k, v))
+        }
+
+        const reqParams = {
+            ...bbParams, ...{
+                redirect_url: encodeURI(redirectUrl.toString()),
+                notify_url: encodeURI(notifyUrl.toString()),
+                value: value,
+                apikey: apiKey
+            }
+        }
+
+        const response = await BlockBee.#_request_get('', 'checkout/request', reqParams)
+        if ( response.status === 'success' ) {
+            return response
+        }
+
+        return null
+    }
+
+    /**
+     * Fetch payment logs
+     * @param {string} token
+     * @param {string} apiKey
+     * @returns {Promise<null|Object>}
+     */
+    static async paymentLogs( token, apiKey ) {
+        if ( !token ) {
+            throw new Error('Token is Empty')
+        }
+
+        const params = {
+            apikey: apiKey,
+            token: token
+        }
+
+        const response = await BlockBee.#_request_get('', 'checkout/logs', params)
+
+        if ( response.status === 'success' ) {
+            return response
+        }
+
+        return null
+    }
+
+    /**
+     * Requests a Deposit Link
+     * @returns {Promise<*|null>}
+     */
+    static async depositRequest( notifyUrl, apiKey, parameters = {}, bbParams = {} ) {
+        if ( !notifyUrl || !apiKey ) {
+            return null
+        }
+
+        notifyUrl = new URL(notifyUrl)
+
+        if ( Object.entries(parameters).length > 0 ) {
+            Object.entries(parameters).forEach(( [k, v] ) => notifyUrl.searchParams.append(k, v))
+        }
+
+        const params = {
+            ...bbParams, ...{
+                notify_url: encodeURI(notifyUrl.toString()),
+                apikey: apiKey
+            }
+        }
+
+        const response = await BlockBee.#_request_get('', 'deposit/request', params)
+
+        if ( response.status === 'success' ) {
+            return response
+        }
+
+        return null
+    }
+
+    /**
+     * Fetch deposit logs
+     * @param {string} token
+     * @param {string} apiKey
+     * @returns {Promise<null|Object>}
+     */
+    static async depositLogs( token, apiKey ) {
+        if ( !token ) {
+            throw new Error('Token is Empty')
+        }
+
+        const params = {
+            apikey: apiKey,
+            token: token
+        }
+
+        const response = await BlockBee.#_request_get('', 'deposit/logs', params)
+
+        if ( response.status === 'success' ) {
+            return response
+        }
+
         return null
     }
 
@@ -266,10 +473,10 @@ class BlockBee {
      * @param params
      * @returns {Promise<any>}
      */
-    static async #_request(coin, endpoint, params = {}) {
+    static async #_request_get( coin, endpoint, params = {} ) {
         const url = coin ? new URL(`${this.#baseURL}/${coin.replace('_', '/')}/${endpoint}/`) : new URL(`${this.#baseURL}/${endpoint}/`)
 
-        if (params) {
+        if ( params ) {
             Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
         }
 
@@ -279,6 +486,39 @@ class BlockBee {
                 referer: this.#baseURL
             },
             credentials: 'include'
+        }
+
+        const response = await fetch(url, fetchParams)
+        return await response.json()
+    }
+
+    static async #_request_post( coin, endpoint, apiKey, body = {}, isJson = false ) {
+        const baseURL = this.#baseURL
+
+        const coinPath = coin ? `${coin.replace('_', '/')}` : ''
+
+        let url = new URL(`${baseURL}/${endpoint}/`)
+
+        if ( coin ) {
+            url = new URL(`${baseURL}/${coinPath}/${endpoint}/`)
+        }
+
+        url.searchParams.append('apikey', apiKey)
+        const headers = {}
+        let data
+
+        if ( isJson ) {
+            headers['Content-Type'] = 'application/json'
+            data = JSON.stringify(body)
+        } else {
+            data = new URLSearchParams(body).toString()
+            headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        }
+
+        const fetchParams = {
+            method: 'POST',
+            headers: headers,
+            body: data
         }
 
         const response = await fetch(url, fetchParams)
--- test.js
+++ test.js
@@ -1,12 +1,11 @@
 const BlockBee = require('./index')
 const test = require('node:test')
 
-const apiKey = '1mzfJHkAf8beGdmGJMhpS9qrm6T9HTcN0foroAGLZQNWzjicMUhP2EgNSM5F1cFY' // <- Insert your API Key here to run tests.
+const apiKey = '' // <- Insert your API Key here to run tests.
 
 test('Test requesting supported cryptocurrencies', async (t) => {
-    const _r = await BlockBee.getSupportedCoins(apiKey)
-
-    if (_r === null) throw new Error('fail')
+    const supportedCoins = await BlockBee.getSupportedCoins(apiKey)
+    if (supportedCoins === null) throw new Error('fail')
 })
 
 test('Test generating address', async (t) => {
@@ -18,7 +17,6 @@ test('Test generating address', async (t) => {
     }, apiKey)
 
     const address = await bb.getAddress()
-
     if (address === null) throw new Error('fail')
 })
 
@@ -31,7 +29,6 @@ test('Test getting logs', async (t) => {
     }, apiKey)
 
     const logs = await bb.checkLogs()
-
     if (logs === null) throw new Error('fail')
 })
 
@@ -42,31 +39,97 @@ test('Test getting QrCode', async (t) => {
         multi_chain: 1,
     }, apiKey)
 
-    /**
-     * First is important to run getAddress otherwise the remaining requests won't function
-     */
-    await bb.getAddress()
-
-    const qrCode = await bb.getQrcode(1, 300)
-
+    const qrCode = await bb.getQrcode(2, 300)
     if (qrCode === null) throw new Error('fail')
 })
 
 test('Test getting getEstimate', async (t) => {
     const estimate = await BlockBee.getEstimate('polygon_matic', apiKey,1, 'default')
-
     if (estimate === null) throw new Error('fail')
 })
 
 test('Test getting getConvert', async (t) => {
     const convert = await BlockBee.getConvert('polygon_matic', 300,'USD', apiKey)
-
+    console.log(convert)
     if (convert === null) throw new Error('fail')
 })
 
 test('Test creating Payout Request', async (t) => {
-    const payout = await BlockBee.createPayout('polygon_matic', '0xA6B78B56ee062185E405a1DDDD18cE8fcBC4395d',1, apiKey)
-    
+    const requests = {
+        '0xA6B78B56ee062185E405a1DDDD18cE8fcBC4395d': 0.5,
+        '0x18B211A1Ba5880C7d62C250B6441C2400d588589': 0.1
+    }
+
+    const payout = await BlockBee.createPayout('polygon_matic', requests, apiKey, false)
     if (payout === null) throw new Error('fail')
 })
 
+test('Test fetching Payout list', async (t) => {
+    const payout = await BlockBee.listPayouts('polygon_matic', '', '', apiKey)
+
+    if (payout === null) throw new Error('fail')
+})
+
+test('Get Payout Wallet / and balance', async (t) => {
+    const wallet = await BlockBee.getPayoutWallet('polygon_matic', apiKey, true)
+
+    if (wallet === null) throw new Error('fail')
+})
+
+test('Create Payout by IDs', async (t) => {
+    const ids = [52211, 52212]
+
+    const payout = await BlockBee.createPayoutByIds(apiKey, ids)
+
+    if (payout === null) throw new Error('fail')
+})
+
+test('Process Payout by ID', async (t) => {
+    const payout = await BlockBee.processPayout(apiKey, 2463)
+
+    if (payout === null) throw new Error('fail')
+})
+
+test('Check Payout status by ID', async (t) => {
+    const status = await BlockBee.checkPayoutStatus(apiKey, 2463)
+    if (status === null) throw new Error('fail')
+})
+
+test('Test generating a payment link', async (t) => {
+    const paymentLink = await BlockBee.paymentRequest(
+        'https://example.com',
+        'https://example.com',
+        10,
+        apiKey,
+        {
+            order_id: 12345
+        }
+    )
+    console.log(paymentLink)
+    if (paymentLink === null) throw new Error('fail')
+})
+
+test('Test fetching payment logs', async (t) => {
+    const token = 'OcRrZGsKQFGsoi0asqZkr97WbitMxFMb'
+    const paymentLogs = await BlockBee.paymentLogs(token, apiKey)
+    if (paymentLogs === null) throw new Error('fail')
+})
+
+test('Test generating a deposit link', async (t) => {
+    const depositLink = await BlockBee.depositRequest(
+        'https://example.com',
+        {
+            deposit_id: 124,
+        },
+        '',
+        apiKey
+    )
+    console.log(depositLink)
+    if (depositLink === null) throw new Error('fail')
+})
+
+test('Test fetching deposit logs', async (t) => {
+    const token = 'jv12du8IWqS96WVDjZK2J285WOBOBycc'
+    const depositLogs = await BlockBee.depositLogs(token, apiKey)
+    if (depositLogs === null) throw new Error('fail')
+})
